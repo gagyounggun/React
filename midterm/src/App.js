5a400e21d0ee571444eed462cc99b7429c4f2b25
gagyounggun <ccc00311@naver.com>
2025-04-24 13:33:38 +0900

    1번째 commit

--- midterm/src/App.js
+++ midterm/src/App.js
@@ -1,7 +1,11 @@
+import Midterm from './Midterm.js';
 import './App.css';
+// App 컴포넌트
+export default function App() {
+  return (
+    <div>
+      <Midterm />
+    </div>
 
-function App() {
-  
+  );
 }
-
-export default App;
---
+++ midterm/src/Midterm.js
@@ -0,0 +1,28 @@
+function MyButton1() {
+  return (
+    <button>사과</button>
+  );
+}
+
+function MyButton2() {
+  return (
+    <button>오렌지</button>
+  );
+}
+
+function MyButton3() {
+  return (
+    <button>바나나</button>
+  );
+}
+
+export default function midterm() {
+  return (
+    <div>
+      <h1>당신은 어떤 과일을 좋아하시나요?</h1>
+      <MyButton1 />
+      <MyButton2 />
+      <MyButton3 />
+    </div>
+  );
+}
